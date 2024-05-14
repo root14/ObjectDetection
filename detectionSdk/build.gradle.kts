7a7907638b065dff2137c773793571f70e77f171
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-05-14 14:10:46 +0300

    add object detection

--- detectionSdk/build.gradle.kts
+++ detectionSdk/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     buildFeatures {
         mlModelBinding = true
     }
+
 }
 
 dependencies {
--- detectionSdk/src/main/java/com/root14/detectionsdk/view/DetectionSurface.kt
+++ detectionSdk/src/main/java/com/root14/detectionsdk/view/DetectionSurface.kt
@@ -3,6 +3,10 @@ package com.root14.detectionsdk.view
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
@@ -16,21 +20,47 @@ import android.view.TextureView
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.root14.detectionsdk.R
+import com.root14.detectionsdk.ml.Detect
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+
 
 class DetectionSurface(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
-
+    lateinit var labels: List<String>
+    var colors = listOf<Int>(
+        Color.BLUE,
+        Color.GREEN,
+        Color.RED,
+        Color.CYAN,
+        Color.GRAY,
+        Color.BLACK,
+        Color.DKGRAY,
+        Color.MAGENTA,
+        Color.YELLOW,
+        Color.RED
+    )
+    private val paint = Paint()
     private lateinit var bitmap: Bitmap
     private lateinit var cameraDevice: CameraDevice
     private lateinit var cameraManager: CameraManager
     private lateinit var textureView: TextureView
     private lateinit var imageView: ImageView
     private lateinit var handler: Handler
+    private lateinit var model: Detect
 
     init {
         inflate(context, R.layout.detection_surface, this)
 
         textureView = findViewById(R.id.textureView)
-        //imageView = findViewById(R.id.imageView)
+        imageView = findViewById(R.id.imageView)
+
+        labels = FileUtil.loadLabels(context, "labelmap.txt")
+
+        model = Detect.newInstance(context)
 
         val handlerThread = HandlerThread("videoThread")
         handlerThread.start()
@@ -51,17 +81,88 @@ class DetectionSurface(context: Context, attrs: AttributeSet?) : ConstraintLayou
             }
 
             override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                // Releases model resources if no longer used.
+                model.close()
                 return false
             }
 
             override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
                 bitmap = textureView.bitmap!!
+
+                val imageResult = TensorImage.fromBitmap(bitmap)
+
+
+                val imageProcessor =
+                    ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR))
+                        .build()
+
+                val result = imageProcessor.process(imageResult)
+
+                // Creates inputs for reference.
+                val inputFeature0 =
+                    TensorBuffer.createFixedSize(intArrayOf(1, 300, 300, 3), DataType.UINT8)
+                inputFeature0.loadBuffer(result.buffer)
+
+                // Runs model inference and gets result.
+                val outputs = model.process(inputFeature0)
+
+                //locations
+                val locations = outputs.outputFeature0AsTensorBuffer.floatArray
+                Log.d("TAG", "outputFeature0: $locations")
+
+                //classes
+                val classes = outputs.outputFeature1AsTensorBuffer.floatArray
+                Log.d("TAG", "outputFeature1: $classes")
+
+                //scores
+                val scores = outputs.outputFeature2AsTensorBuffer.floatArray
+                Log.d("TAG", "outputFeature2: $scores")
+
+                //number of detections
+                val numberOfDetection = outputs.outputFeature3AsTensorBuffer.floatArray
+                Log.d("TAG", "outputFeature3: $numberOfDetection")
+
+
+                val mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+
+
+                val h = mutable.height
+                val w = mutable.width
+                paint.textSize = h / 15f
+                paint.strokeWidth = h / 85f
+                var x = 0
+                scores.forEachIndexed { index, fl ->
+                    x = index
+                    x *= 4
+                    if (fl > 0.5) {
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(
+                            RectF(
+                                locations[x + 1] * w,
+                                locations[x] * h,
+                                locations[x + 3] * w,
+                                locations[x + 2] * h
+                            ), paint
+                        )
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(
+                            labels[classes[index].toInt()] + " " + fl.toString(),
+                            locations[x + 1] * w,
+                            locations[x] * h,
+                            paint
+                        )
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
+
             }
 
         }
 
         cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
     }
 
     private fun initView(context: Context) {
@@ -71,8 +172,7 @@ class DetectionSurface(context: Context, attrs: AttributeSet?) : ConstraintLayou
     @SuppressLint("MissingPermission")
     fun openCamera() {
         cameraManager.openCamera(
-            cameraManager.cameraIdList[0],
-            object : CameraDevice.StateCallback() {
+            cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
                 override fun onOpened(p0: CameraDevice) {
                     cameraDevice = p0
 
@@ -84,16 +184,14 @@ class DetectionSurface(context: Context, attrs: AttributeSet?) : ConstraintLayou
                     captureRequest.addTarget(surface)
 
                     cameraDevice.createCaptureSession(
-                        listOf(surface),
-                        object : CameraCaptureSession.StateCallback() {
+                        listOf(surface), object : CameraCaptureSession.StateCallback() {
                             override fun onConfigured(p0: CameraCaptureSession) {
                                 p0.setRepeatingRequest(captureRequest.build(), null, null)
                             }
 
                             override fun onConfigureFailed(p0: CameraCaptureSession) {
                             }
-                        },
-                        handler
+                        }, handler
                     )
                 }
 
@@ -104,8 +202,7 @@ class DetectionSurface(context: Context, attrs: AttributeSet?) : ConstraintLayou
                 override fun onError(p0: CameraDevice, p1: Int) {
 
                 }
-            },
-            handler
+            }, handler
         )
     }
 
